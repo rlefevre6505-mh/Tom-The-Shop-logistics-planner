9a4f001fe3b5eae07fae933115a8caaa46a1a779
Richard Lefevre <rlefevre.6505@gmail.com>
2026-06-10 15:24:02 +0100

    updated logo file name

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,6 +2,6 @@ import type { JSX } from "react";
 
 export default function Logo(): JSX.Element {
   return (
-    <img className="logo" src="src/assets/logo.webp" alt="Tom The Shop logo" />
+    <img className="logo" src="src/assets/Logo.webp" alt="Tom The Shop logo" />
   );
 }
