6f92ac41552c1e3ae23556fc731a6689bbe1f39a
Richard Lefevre <rlefevre.6505@gmail.com>
2026-06-10 15:28:36 +0100

    changed image import

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,6 @@
 import type { JSX } from "react";
+import LogoImg from "../assets/Logo.webp";
 
 export default function Logo(): JSX.Element {
-  return (
-    <img className="logo" src="src/assets/Logo.webp" alt="Tom The Shop logo" />
-  );
+  return <img className="logo" src={LogoImg} alt="Tom The Shop logo" />;
 }
